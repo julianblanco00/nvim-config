27b279e3d5454ddc143f5bf5e6c4bf7fd91e0be9
Julian Blanco <julianblanco550@gmail.com>
2024-12-23 16:41:21 -0300

    working on formatter

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -20,73 +20,54 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+	require("plenary.reload").reload_module(name)
 end
 
 vim.filetype.add({
-    extension = {
-        templ = 'templ',
-    }
+	extension = {
+		templ = 'templ',
+	}
 })
 
 autocmd("BufReadPost", {
-  pattern = "*",
-  callback = function()
-    vim.cmd("set scroll=5")
-  end,
+	pattern = "*",
+	callback = function()
+		vim.cmd("set scroll=5")
+	end,
 })
 
 autocmd("BufWritePost", {
-  pattern = "*",
-  callback = function()
-    vim.cmd("set scroll=5")
-  end,
-})
-
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
-})
-
-autocmd({"BufWritePre"}, {
-    group = lspGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+	pattern = "*",
+	callback = function()
+		vim.cmd("set scroll=5")
+	end,
 })
 
 autocmd('BufEnter', {
-    group = lspGroup,
-    callback = function()
-        if vim.bo.filetype == "zig" then
-            vim.cmd.colorscheme("tokyonight-night")
-        else
-            vim.cmd.colorscheme("rose-pine-moon")
-        end
-    end
+	callback = function()
+		if vim.bo.filetype == "zig" then
+			vim.cmd.colorscheme("tokyonight-night")
+		else
+			vim.cmd.colorscheme("rose-pine-moon")
+		end
+	end
 })
 
-
 autocmd('LspAttach', {
-    group = lspGroup,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    end
+	group = lspGroup,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+		vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+		vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+		vim.keymap.set("n", "<leader>cd", function() vim.diagnostic.open_float() end, opts)
+		vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+		vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+		vim.keymap.set("n", "<leader>cr", function() vim.lsp.buf.rename() end, opts)
+		vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+		vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+		vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+	end
 })
 
 vim.g.netrw_browse_split = 0
