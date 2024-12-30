1887a7aa5486e9ca79c307a24f159a8e165873fc
Julian Blanco <julianblanco550@gmail.com>
2024-12-30 08:52:33 -0300

    config neo-tree

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,29 +4,29 @@ local defaults = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set('v', '<C-n>', 'y/<C-r>"<CR>Ncgn', defaults)
+vim.keymap.set("v", "<C-n>", 'y/<C-r>"<CR>Ncgn', defaults)
 
-vim.keymap.set({'n', 'v'}, '<leader>dd', '"_dd<Esc>', defaults)
-vim.keymap.set({'n', 'v'}, '<leader>dw', '"_dw<Esc>', defaults)
-vim.keymap.set({'n', 'v'}, '<leader>p', '"_dP<Esc>', defaults)
+vim.keymap.set({ "n", "v" }, "<leader>dd", '"_dd<Esc>', defaults)
+vim.keymap.set({ "n", "v" }, "<leader>dw", '"_dw<Esc>', defaults)
+vim.keymap.set({ "n", "v" }, "<leader>p", '"_dP<Esc>', defaults)
 
-vim.keymap.set('n', '{', '<C-d>', defaults)
-vim.keymap.set('n', '}', '<C-u>', defaults)
+vim.keymap.set("n", "{", "<C-d>", defaults)
+vim.keymap.set("n", "}", "<C-u>", defaults)
 
-vim.keymap.set('n', '<C-e>', '5<C-e>', defaults)
-vim.keymap.set('n', '<C-y>', '5<C-y>', defaults)
+vim.keymap.set("n", "<C-e>", "5<C-e>", defaults)
+vim.keymap.set("n", "<C-y>", "5<C-y>", defaults)
 
-vim.keymap.set('n', '0', '^', defaults)
+vim.keymap.set("n", "0", "^", defaults)
 
-vim.keymap.set('n', 'yp', '<S-v>$%y', defaults)
+vim.keymap.set("n", "yp", "<S-v>$%y", defaults)
 
-vim.keymap.set("n", "<leader>e", "<cmd>:Neotree toggle<CR>")
+vim.keymap.set("n", "<leader>e", "<cmd>:Neotree toggle reveal<CR>")
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+	vim.cmd("so")
 end)
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-vim.keymap.set('n', '<C-h>', '<C-w><Left>', defaults)
-vim.keymap.set('n', '<C-l>', '<C-w><Right>', defaults)
+vim.keymap.set("n", "<C-h>", "<C-w><Left>", defaults)
+vim.keymap.set("n", "<C-l>", "<C-w><Right>", defaults)
